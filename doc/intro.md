5ca1761524762e87452ee411805e8580e5dd3292
Ibrahim Albarghouthi <ibrahimalbarghouthii@hotmail.com>
2017-11-29 22:50:10 +0200

    Demo clojure ci

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ci-demo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ci_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns ci-demo.core-test
+  (:require [clojure.test :refer :all]
+            [ci-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
